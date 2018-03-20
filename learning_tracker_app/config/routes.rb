179cc5c3447d23a386092ea4043a7a80e53e3c52
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-03-20 15:01:16 +0000

    Initial react front end setup created

--- learning_tracker_app/config/routes.rb
+++ learning_tracker_app/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  # get( { 'learned_lessons' => 'learned_lessons#index' } )
   scope path: "api" do
     resources :learned_lessons
   end
--- learning_tracker_app/db/schema.rb
+++ learning_tracker_app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180320105933) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "learned_lessons", force: :cascade do |t|
     t.string "description"
     t.text "blocker"

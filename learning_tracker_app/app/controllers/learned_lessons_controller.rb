20f0132cb8fe2b33b1ff6469bed99c7bbf572ccc
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-03-20 12:24:27 +0000

    All CRUD actions for LearnedLesson - tested in Insomnia + index/show routes displayed render json to browser

---
+++ learning_tracker_app/app/controllers/learned_lessons_controller.rb
@@ -0,0 +1,44 @@
+class LearnedLessonsController < ApplicationController
+
+  def lesson_params
+    params.require(:learned_lesson).permit([:description, :blocker, :resolution])
+  end
+
+  def index
+    @learned_lessons = LearnedLesson.all
+    render :json => @learned_lessons
+    # if I add a method to learned_lesson model - use & update line below replacing
+    # ':method' with the method name. same applies to show route aswell.
+    # render :json => @learned_lessons.as_json(methods: :method)
+  end
+
+  def show
+    @learned_lesson = LearnedLesson.find(params[:id])
+    render :json => @learned_lesson
+  end
+
+  def create
+    learned_lesson = LearnedLesson.create(lesson_params)
+    render :json => learned_lesson
+  end
+
+  def update
+    learned_lesson = LearnedLesson.find(params[:id])
+
+    if learned_lesson.update_attributes(lesson_params)
+      render :json => learned_lesson
+    else
+      render :json => {status: :update_failed}
+    end
+  end
+
+  def destroy
+    learned_lesson = LearnedLesson.find(params[:id])
+
+    if learned_lesson.destroy!
+      render :json => { status: :success  }
+    else
+      render :json => { status: :delete_failed }
+    end
+  end
+end
--- learning_tracker_app/config/routes.rb
+++ learning_tracker_app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get( { 'learned_lessons' => 'learned_lessons#index' } )
+  scope path: "api" do
+    resources :learned_lessons
+  end
+
 end
